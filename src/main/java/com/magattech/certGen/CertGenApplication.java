8123f63b954bcdd39d03354e15748cc1d6ea99b2
Jovan Stankovic <j.stankovic001@gmail.com>
2023-12-26 10:43:14 +0100

    Initial commit.

---
+++ src/main/java/com/magattech/certGen/CertGenApplication.java
@@ -0,0 +1,13 @@
+package com.magattech.certGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magattech/certGen/CertGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magattech.certGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
